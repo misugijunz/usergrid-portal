360e5af54bcece625ac9e0fe912587d224b638b7
Oscar Kurniawan <misugijunz@gmail.com>
2016-02-22 03:06:33 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 if [ -n "${USERGRID_HOST}" ]; then
   # patch url where the portal can find the usergrid api if USERGRID_HOST is set
-  sed -i "s#Usergrid.overrideUrl = 'http://localhost:8080/';#Usergrid.overrideUrl = 'http://${USERGRID_HOST}/';#g" /var/www/html/config.js
+  sed -i "s#Usergrid.overrideUrl = 'http://localhost:8080/';#Usergrid.overrideUrl = 'http://${USERGRID_HOST}/';#g" /root/usergrid/portal/config.js
 fi
 
